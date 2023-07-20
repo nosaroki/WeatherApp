a30ef8f98c56598fd9274a1225af96f1ee9d738c
Nnéna <nnena.osarokio@hotmail.fr>
2023-07-20 17:18:00 +0200

    init project

---
+++ WeatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherApp2
+//
+//  Created by Nnéna Okrs on 20/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp2App.swift
+//  WeatherApp2
+//
+//  Created by Nnéna Okrs on 20/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
